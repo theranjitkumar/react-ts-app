51c3cf086cd8275454daefb98b983be79b401a35
theranjitkumar <ranjit.kumar@mobileprogramming.net>
2019-03-09 14:00:54 +0530

    store > index.ts updated

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,29 @@
-import {createStore, combineReducers, Store } from 'redux';
+import {createStore, combineReducers } from 'redux';
 import { postReducer } from './posts/reducers'
 
+function counter(state = 0, action:any) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
 const rootReducer = combineReducers({
-  post: postReducer
+  post: postReducer,
+  counter: counter
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer);
 
-store.getState();
+store.subscribe(() => console.log(store.getState()))
+
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'DECREMENT' })
+
 
 export default  store; 
